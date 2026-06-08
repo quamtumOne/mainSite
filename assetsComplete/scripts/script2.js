6d0070de98d426563d151cc8afe231d17f704e61
sirlupinwatson <55941045+sirlupinwatson@users.noreply.github.com>
2026-06-08 01:25:53 -0400

    feat: implement sitemap.xml schema and deploy picxbits proto-modus intro

--- assetsComplete/scripts/script2.js
+++
@@ -1,7 +0,0 @@
-    
-<script>
-<script src=""></script>
-<script src="https://cdn.tailwindcss.com"></script>
-<script src="https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.min.js"></script>
-</script>
-
